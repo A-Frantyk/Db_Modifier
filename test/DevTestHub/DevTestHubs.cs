79dd6fcb594de702a27335cd0428052e67ffa5c9
afrantyk <ifrantya@gmail.com>
2017-01-12 22:52:02 +0200

    add signalr hub

---
+++ test/DevTestHub/DevTestHubs.cs
@@ -0,0 +1,23 @@
+﻿using System.Configuration;
+using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
+
+namespace test.DevTestHub
+{
+    public class DevTestHubs : Hub
+    {
+        private static string conString = ConfigurationManager.ConnectionStrings["DbTestContext"].ToString();
+
+        public void Hello()
+        {
+            Clients.All.hello();
+        }
+
+        [HubMethodName("sendMessages")]
+        public static void SendMessages()
+        {
+            IHubContext context = GlobalHost.ConnectionManager.GetHubContext<DevTestHubs>();
+            context.Clients.All.updateMessages();
+        }
+    }
+}
